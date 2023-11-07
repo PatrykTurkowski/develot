efed96ad7028650801c3528f0c9fffb0438bebb6
patryk <patryk.turkowski21@gmail.com>
2023-11-07 19:57:08 +0100

    update composer

--- resources/views/includes/_links.blade.php
+++ resources/views/includes/_links.blade.php
@@ -1,6 +1,2 @@
-@vite(['resources/scss/app.scss', 'resources/js/app.js'])
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 <script src="https://kit.fontawesome.com/36040ca96e.js" crossorigin="anonymous"></script>
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swap" rel="stylesheet">
